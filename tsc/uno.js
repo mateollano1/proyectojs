93544ef694ab63038877e0fc323ed27b246873b7
Mateo Llano <mateo.llano1@gmail.com>
2018-09-16 17:39:29 -0500

    Continuar con promesas enES6 video 18

--- tsc/uno.js
+++ tsc/uno.js
@@ -1,15 +1,34 @@
-console.log("soy el primero");
+//console.log("soy el primero"); 
 var mensaje = "Mateo";
 var numero = 15;
 var tyu = true;
 var EDAD = 15;
 var texto = "hola " + mensaje + "\nedad " + EDAD;
-console.log(texto);
+//console.log(texto);
 function saludar(nombre) {
     if (nombre) {
         console.log("hola " + nombre + " \n\u00BFcomo estas?");
     }
 }
-saludar("josè");
+//---- funciones flecha ----
+//saludar("josè");
 var comer = function (vo) { return (EDAD); };
-comer();
+//comer("");
+var probar = function (a, b) {
+    console.log("probando " + b + " en el numero " + a);
+};
+//probar(5,"teclado");
+//----Desestructuraciòn---- 
+var jugador = {
+    nombre: "Radamel",
+    apellido: "Falcao",
+    posicion: "Delantero"
+};
+var jugador2 = {
+    nombre: "Davinson",
+    apellido: "Sanchez",
+    posicion: "Defensa"
+};
+//let {nombre,apellido,posicion}=jugador;
+var nombre = jugador2.nombre, apellido = jugador2.apellido, posicion = jugador2.posicion;
+console.log("El jugador " + nombre + " de apellido " + apellido + "\njuega en la posici\u00F2n de " + posicion);
--- tsc/uno.ts
+++ tsc/uno.ts
@@ -1,17 +1,39 @@
-console.log("soy el primero"); 
+//console.log("soy el primero"); 
 let mensaje:string = "Mateo";
 let numero:number =15;
 let tyu:boolean=true;
 const EDAD =15;
 let texto:string=`hola ${mensaje}
 edad ${EDAD}`;
-console.log(texto);
+//console.log(texto);
 function saludar(nombre?:string){
     if (nombre){
         console.log(`hola ${nombre} 
 ¿como estas?`);
     }
 }
-saludar("josè");
+
+//---- funciones flecha ----
+//saludar("josè");
 let comer = (vo:string) =>(EDAD);
-comer();
+//comer("");
+let probar = (a:number,b:string)=>{
+    console.log(`probando ${b} en el numero ${a}`);
+};
+//probar(5,"teclado");
+
+//----Desestructuraciòn---- 
+let jugador ={
+    nombre: "Radamel",
+    apellido: "Falcao",
+    posicion:"Delantero"
+}
+let jugador2 ={
+    nombre: "Davinson",
+    apellido: "Sanchez",
+    posicion:"Defensa"
+}
+//let {nombre,apellido,posicion}=jugador;
+let {nombre,apellido,posicion}=jugador2;
+console.log(`El jugador ${nombre} de apellido ${apellido}
+juega en la posiciòn de ${posicion}`); 
